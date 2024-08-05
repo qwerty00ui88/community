78daff30948fd9281e98cb9133673b66f0712c60
hameee <113837605+qwerty00ui88@users.noreply.github.com>
2024-08-05 18:09:51 +0900

    chore: 초기 환경 설정(aws ec2)

--- src/main/java/com/community/CommunityApplication.java
+++ src/main/java/com/community/CommunityApplication.java
@@ -2,9 +2,10 @@ package com.community;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CommunityApplication {
+public class CommunityApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CommunityApplication.class, args);
