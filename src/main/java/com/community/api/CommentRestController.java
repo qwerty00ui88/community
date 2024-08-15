a3f3c141f57540b10ffbabe2ac24dcbf6c5583f3
hameee <113837605+qwerty00ui88@users.noreply.github.com>
2024-08-16 03:17:54 +0900

    feat: 댓글 관련 로직 추가

--- src/main/java/com/community/api/CommentRestController.java
+++ src/main/java/com/community/api/CommentRestController.java
@@ -1,76 +1,75 @@
 package com.community.api;
 
-import org.springframework.http.HttpStatus;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.community.aop.LoginCheck;
-import com.community.dto.CommentDTO;
-import com.community.dto.UserDTO;
 import com.community.dto.response.CommonResponse;
 import com.community.entity.CommentEntity;
-import com.community.service.impl.CommentServiceImpl;
-import com.community.service.impl.UserServiceImpl;
+import com.community.service.CommentService;
 
-@RequestMapping("/api/comment")
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(name = "댓글 관련 API")
 @RestController
+@RequestMapping("/api/comment")
 public class CommentRestController {
 
-	private final CommentServiceImpl commentService;
-
-	public CommentRestController(CommentServiceImpl commentService) {
-		this.commentService = commentService;
-	}
+	@Autowired
+	private CommentService commentService;
 
 	// 댓글 생성
 	@LoginCheck
 	@PostMapping
+	@Operation(summary = "댓글 생성")
 	public ResponseEntity<CommonResponse<CommentEntity>> createComment(
 			@RequestParam(name = "id", required = false) Integer userId, @RequestParam("postId") int postId,
 			@RequestParam(name = "parentId", required = false) Integer parentId,
 			@RequestParam("contents") String contents) {
 		CommentEntity comment = commentService.createComment(userId, postId, parentId, contents);
-		CommonResponse commonResponse = new CommonResponse<CommentEntity>(HttpStatus.OK, "SUCCESS", "댓글 생성 성공",
-				comment);
+		CommonResponse<CommentEntity> commonResponse = CommonResponse.success("댓글 생성 성공", comment);
 		return ResponseEntity.ok(commonResponse);
 	}
 
 	// 댓글 수정
 	@LoginCheck
 	@PutMapping("/{commentId}")
-	public ResponseEntity<CommonResponse> updateComment(@RequestParam(name = "id", required = false) Integer userId,
-			@PathVariable("commentId") int commentId, @RequestParam("contents") String contents) {
+	@Operation(summary = "댓글 수정")
+	public ResponseEntity<CommonResponse<CommentEntity>> updateComment(
+			@RequestParam(name = "id", required = false) Integer userId, @PathVariable("commentId") int commentId,
+			@RequestParam("contents") String contents) {
 		CommentEntity comment = commentService.updateCommentByIdAndUserId(commentId, userId, contents);
-		CommonResponse commonResponse = new CommonResponse<CommentEntity>(HttpStatus.OK, "SUCCESS", "댓글 수정 성공",
-				comment);
+		CommonResponse<CommentEntity> commonResponse = CommonResponse.success("댓글 수정 성공", comment);
 		return ResponseEntity.ok(commonResponse);
 	}
 
 	// 댓글 삭제
 	@LoginCheck
 	@DeleteMapping("/{commentId}")
-	public ResponseEntity<CommonResponse> deleteCommentByIdAndUserId(
+	@Operation(summary = "댓글 삭제")
+	public ResponseEntity<CommonResponse<CommentEntity>> deleteCommentByIdAndUserId(
 			@RequestParam(name = "id", required = false) Integer userId, @PathVariable("commentId") int commentId) {
 		commentService.deleteCommentByIdAndUserId(commentId, userId);
-		CommonResponse commonResponse = new CommonResponse<CommentEntity>(HttpStatus.OK, "SUCCESS", "댓글 삭제 성공", null);
+		CommonResponse<CommentEntity> commonResponse = CommonResponse.success("댓글 삭제 성공", null);
 		return ResponseEntity.ok(commonResponse);
 	}
 
 	// 관라자용 댓글 삭제
 	@LoginCheck(type = LoginCheck.UserType.ADMIN)
 	@DeleteMapping("/admin/{commentId}")
-	public ResponseEntity<CommonResponse> deleteCommentById(@RequestParam(name = "id", required = false) Integer userId,
-			@PathVariable("commentId") int commentId) {
+	@Operation(summary = "(관리자용) 댓글 삭제")
+	public ResponseEntity<CommonResponse<Void>> deleteCommentById(
+			@RequestParam(name = "id", required = false) Integer userId, @PathVariable("commentId") int commentId) {
 		commentService.deleteCommentById(commentId);
-		CommonResponse commonResponse = new CommonResponse<CommentEntity>(HttpStatus.OK, "SUCCESS", "댓글 삭제 성공", null);
+		CommonResponse<Void> commonResponse = CommonResponse.success("댓글 삭제 성공", null);
 		return ResponseEntity.ok(commonResponse);
 	}
 
--- src/main/java/com/community/dto/CommentDTO.java
+++ src/main/java/com/community/dto/CommentDTO.java
@@ -1,8 +1,9 @@
 package com.community.dto;
 
 import java.time.ZonedDateTime;
-import java.util.ArrayList;
 import java.util.List;
+
+import com.community.enums.CommentStatus;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -23,6 +24,7 @@ public class CommentDTO {
 	private int userId;
 	private Integer parentId;
 	private String contents;
+	private CommentStatus status;
 	private ZonedDateTime createdAt;
 	private ZonedDateTime updatedAt;
 	private UserDTO writer;
